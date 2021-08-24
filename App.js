daf6a5cbca30c437fcc0f06f6fa865969910cc61
arunkumar-thirumalai <78794317+arunkumar-thirumalai@users.noreply.github.com>
2021-08-24 17:35:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import './App.css';
+import InputArea from './InputArea';
+import ToDoItem from './ToDoItem';
+
+function App() {
+  const[items,setItems] = useState([]);
+
+ const addItems = (inputText)=>{
+   setItems((prevItems)=>{
+     return [...prevItems, inputText]
+   });
+  };
+
+ const deleteItem = (id)=>{
+   setItems((prevItems)=>{
+     return prevItems.filter((item,index)=>{
+      return index!==id;
+    });
+  });
+ };
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do-List</h1>
+      </div>
+      <InputArea addItems={addItems}></InputArea>
+    <div>
+      <ul>
+        {
+          items.map((item,index)=>{
+            return <ToDoItem key={index} text={item} deleteItem={deleteItem} id={index}/>;
+          })
+        }
+      </ul>
+    </div>
+    </div>
+  );
+};
+
+export default App;
